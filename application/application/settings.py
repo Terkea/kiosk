2deedb0225374efe55da14c3801fede6586d7fdf
Terkea <terkeabt@gmail.com>
2020-03-10 05:54:01 +0000

    docker db and connectivity almost there

--- application/application/settings.py
+++ application/application/settings.py
@@ -74,13 +74,25 @@ WSGI_APPLICATION = 'application.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.oracle',
+#         'NAME': 'localhost:1521/ORCLCDB.LOCALDOMAIN',
+#         'USER': 'terkea',
+#         'PASSWORD': 'test',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.oracle',
+            'NAME': '127.0.0.1:1521/ORCLCDB.LOCALDOMAIN',
+            'USER': 'terkea',
+            'PASSWORD': 'test',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
